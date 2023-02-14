367529cdf94ae6c066784546efb738048117ce50
rqin525 <ruiqian.qin@gmail.com>
2023-02-13 16:42:55 -0800

    Update App.js
    removed css from App.js

--- App.js
+++ App.js
@@ -90,23 +90,6 @@ const App: () => Node = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+
 
 export default App;
