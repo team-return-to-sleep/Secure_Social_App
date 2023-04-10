4b1bd87e814469b2c0a0fdde75011164ced3d47c
Jessica Cai <jessica7cai@gmail.com>
2023-04-10 16:30:02 -0700

    implemented favorites selection

--- screens/Profile/ProfileFavorites.js
+++ screens/Profile/ProfileFavorites.js
@@ -1,61 +1,123 @@
 import * as React from 'react';
-import { Appbar,Title,Button,TextInput} from 'react-native-paper';
-import {View,Text,SafeAreaView,StyleSheet,ScrollView} from 'react-native'
+import { Appbar, Title,Button,TextInput} from 'react-native-paper';
+import {View,Text,SafeAreaView,StyleSheet,TouchableHighlight} from 'react-native'
+
+import {updateUser} from '../../src/graphql/mutations'
+import {API, graphqlOperation} from '@aws-amplify/api'
 
 import Header from '../Header'
 
-const ProfileFavorites = ({navigation}) => {
+const ProfileFavorites = ({route, navigation}) => {
+    const {user, interests} = route.params;
+
+    var [ userInterests , setUserInterests ] = React.useState(interests);
+
+    var favorites = {}
+    interests.forEach(interest => {
+        if (!user.favoriteInterests || !(user.favoriteInterests.includes(interest))) {
+            favorites[interest] = false;
+        }
+        else {
+            favorites[interest] = true;
+        }
+    });
+
+    var [ favoriteInterests , setFavoriteInterests ] = React.useState(favorites);
+
+    var [ isPress, setIsPress ] = React.useState(false);
+
+   const saveUpdates = async() => {
+    var updatedFavoriteInterests = Object.keys(favoriteInterests).filter(interest => favoriteInterests[interest] === true)
+    await API.graphql (
+    {
+        query: updateUser,
+        variables: {
+            input: {
+                id: user.id,
+                favoriteInterests: updatedFavoriteInterests
+            }
+        },
+        authMode: "API_KEY"
+    })
+    navigation.navigate("Account");
+    }
+
     return (
+        <View style={styles.container}>
 
-        <ScrollView style={styles.container}>
-            <Appbar.Header>
-                <Appbar.BackAction onPress={() => navigation.goBack()} />
-                <Title>
-                    Edit Profile
-                </Title>
-            </Appbar.Header>
+                <Appbar.Header>
+                    <Appbar.BackAction onPress={() => navigation.goBack()} />
+                    <Title>
+                        Interests
+                    </Title>
+                </Appbar.Header>
 
-            <Text style={styles.question}> What are your favorites? </Text>
-            <View style={styles.interestsWrapper}>
-                <View style={styles.interests}/>
-                <View style={styles.interests}/>
-                <View style={styles.interests}/>
-                <View style={styles.interests}/>
-                <View style={styles.interests}/>
-            </View>
-            <Button icon="content-save"
-            mode="contained"
-            style={styles.nextButton}
-            onPress={() => navigation.navigate("Account")}>
-                Continue
-            </Button>
-            <View style={{marginBottom:26}}>
-               <Text>  {'\n\n'} </Text>
-            </View>
-        </ScrollView>
-
+                <Text style={styles.question}> What are your favorite interests? </Text>
+                <View style={styles.interestsWrapper}>
+                    {Object.keys(favoriteInterests).map((interest) => {
+                        return (
+                            <TouchableHighlight
+                              activeOpacity = {1}
+                              underlayColor = {'#ffffff'}
+                              style = {favoriteInterests[interest] ? styles.selected : styles.interests}
+                              onPress={() => {
+                                var temp = {...favoriteInterests}
+                                if(favoriteInterests[interest]) {
+                                    temp[interest] = false;
+                                } else {
+                                    temp[interest] = true;
+                                }
+                                setFavoriteInterests(temp)
+                              }}
+                              >
+                              <Text>{interest}</Text>
+                            </TouchableHighlight>
+                        );
+                    })}
+                </View>
+                <Button icon="content-save"
+                mode="contained"
+                style={styles.nextButton}
+                onPress={() => saveUpdates()}>
+                    Save
+                </Button>
+        </View>
       );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
-            flex:1,
-            backgroundColor:'#FFFFFF',
+        flex:1,
+        backgroundColor:'#FFFFFF',
     },
     interests: {
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 10,
-        marginRight: 10,
-        width: 120,
-        height: 120,
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 5,
+        marginRight: 5,
+        width: 100,
+        height: 40,
         alignItems: 'center',
-        justifyContent: 'space-evenly',
-        backgroundColor: '#ffffff',
+        justifyContent: 'center',
         borderRadius: 24,
+        backgroundColor: '#ffffff',
         borderWidth: 3,
         borderColor: '#DDEDEA',
+        color: 'black',
+    },
+    selected: {
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 5,
+        marginRight: 5,
+        width: 100,
+        height: 40,
         alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 24,
+        backgroundColor: '#DDEDEA',
+        borderWidth: 3,
+        borderColor: '#DDEDEA',
     },
     interestsWrapper: {
         flex: 1,
@@ -63,7 +125,8 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: 20,
+        marginTop: 20,
+        marginBottom: 20,
     },
     question: {
         paddingTop: 20,
@@ -71,10 +134,9 @@ const styles = StyleSheet.create({
         fontSize: 18,
     },
     nextButton: {
-        marginTop: '5%',
         width: 150,
         alignSelf: 'center',
-        marginBottom: '15%',
+        marginBottom: '25%',
         backgroundColor: '#BBCAEB',
     },
 })
